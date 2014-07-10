2a390b2e70b0c5eefc01b61574f8abe03b1e4fea
Gaurav Asati <gasati@codeaurora.org>
2014-07-10 16:28:49 +0530

    Bluetooth: Update Current Call list.
    Update current call list on CLCC response
    from AG.
    
    Change-Id: I93c07c48f1c84234a5287863f88fe353a288b928
    CRs-Fixed: 694463

--- src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
+++ src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
@@ -838,7 +838,9 @@ final class HeadsetClientStateMachine extends StateMachine {
         while (it.hasNext()) {
             Hashtable.Entry<Integer, BluetoothHeadsetClientCall> entry = it.next();
 
-            if (mCallsUpdate.containsKey(entry.getKey())) {
+            if (mCallsUpdate.containsKey(entry.getKey()) ||
+                    entry.getValue().getState() ==
+                    BluetoothHeadsetClientCall.CALL_STATE_HELD_BY_RESPONSE_AND_HOLD) {
                 continue;
             }
 
@@ -884,6 +886,11 @@ final class HeadsetClientStateMachine extends StateMachine {
             boolean outgoing) {
         Log.d(TAG, "queryCallsUpdate: " + id);
 
+        if (!mCalls.containsKey(id)) {
+            Log.d(TAG, "adding call " + id);
+            mCalls.put(id, new BluetoothHeadsetClientCall(mCurrentDevice, id, state, number, multiParty,
+                    outgoing));
+        }
         // should not happen
         if (mCallsUpdate == null) {
             return;
